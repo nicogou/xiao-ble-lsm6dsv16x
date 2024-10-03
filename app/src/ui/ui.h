829487b46e7b5b3604ee793df2885799719b1f49
Nicolas Goualard <nicolas.goualard@groupevoltaire.com>
2024-10-03 16:42:06 +0200

    Added a ui file for handling led

---
+++ app/src/ui/ui.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/pwm.h>
+
+#define STACK_SIZE_UI 512
+
+#define STEP_SIZE PWM_USEC(2000)
+#define UI_COLOR_MAX 10000
+#define UI_LED_INTENSITY_LOW 200
+#define UI_LED_BLINK_MAX 100
+
+typedef enum {
+    UI_BTN_SHORTPRESS,
+    UI_BTN_LONGPRESS,
+} ui_instruction_type_t;
+
+typedef struct
+{
+    uint16_t red;   /* Intensity 0-UI_COLOR_MAX */
+    uint16_t green; /* Intensity 0-UI_COLOR_MAX */
+    uint16_t blue;  /* Intensity 0-UI_COLOR_MAX */
+    uint8_t blink;  /* Ratio off_duration/on_duration in % */
+    uint8_t duration; /* blink cycle length in seconds */
+} ui_rgb_t;
+
+uint8_t ui_init();
+uint8_t ui_set_rgb_on(ui_rgb_t *color);
+uint8_t ui_set_rgb_off();
+
+void button_resetsoft();
+void button_resetall();
