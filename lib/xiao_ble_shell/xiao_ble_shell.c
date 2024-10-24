e7acd733860538711d1d57ca5a15f8220575d570
Nicolas Goualard <nicolas.goualard@sfr.fr>
2024-10-24 23:27:11 +0200

    Use date_time library from SDK to set/get GMT time

--- lib/xiao_ble_shell/xiao_ble_shell.c
+++ lib/xiao_ble_shell/xiao_ble_shell.c
@@ -1,4 +1,6 @@
 #include <zephyr/shell/shell.h>
+#include <date_time.h>
+#include <stdlib.h>
 
 static int cmd_demo_ping(const struct shell *sh, size_t argc, char **argv)
 {
@@ -119,3 +121,51 @@ static int cmd_reset(const struct shell *sh, size_t argc, char **argv)
 }
 
 SHELL_CMD_REGISTER(reset, NULL, "Reset Xiao BLE.", cmd_reset);
+
+static int cmd_set_time(const struct shell *sh, size_t argc, char **argv)
+{
+	shell_warn(sh, "WARNING: this function sets the GMT time!");
+	const struct tm now = {.tm_hour = strtol(argv[1], NULL, 10), .tm_min = strtol(argv[2], NULL, 10), .tm_sec = strtol(argv[3], NULL, 10), .tm_mday = strtol(argv[4], NULL, 10), .tm_mon = strtol(argv[5], NULL, 10) - 1, .tm_year = strtol(argv[6], NULL, 10) - 1900};
+	int res = date_time_set(&now);
+	if (res)
+	{
+		shell_error(sh, "Formatting error while setting time!");
+		return res;
+	}
+
+	int64_t t;
+	res = date_time_now(&t);
+	if (res)
+	{
+		shell_error(sh, "Cannot get current time (%i)", res);
+		return res;
+	}
+	shell_print(sh, "Current time %llu", t);
+
+	return 0;
+}
+
+static int cmd_get_time(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	int64_t t;
+	int res = date_time_now(&t);
+	if (res)
+	{
+		shell_error(sh, "Error while retrieving time (%i)", res);
+		return res;
+	}
+
+	shell_print(sh, "Current time %llu", t);
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_date,
+	SHELL_CMD_ARG(set, NULL, "Set GMT time. Format: time set hour(0-23) minutes(0-59) seconds(0-59) day(1-31) month(1-12) year(>2015)", cmd_set_time, 7, 0),
+	SHELL_CMD(get, NULL, "Get GMT time.", cmd_get_time),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+
+SHELL_CMD_REGISTER(time, &sub_date, "Get/Set Xiao BLE time.", NULL);
