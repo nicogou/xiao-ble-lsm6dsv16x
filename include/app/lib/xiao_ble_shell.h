393713f636e0989d7428aa16da3cc8fe9e93a1b9
Nicolas Goualard <nicolas.goualard@sfr.fr>
2024-10-25 18:06:44 +0200

    Communalize reset into bootloader code.

---
+++ include/app/lib/xiao_ble_shell.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <zephyr/kernel.h>
+
+#define ADAFRUIT_BOOTLOADER_UF2 0x57
+#define ADAFRUIT_BOOTLOADER_OTA 0xA8
+#define ADAFRUIT_BOOTLOADER_SERIAL 0x4E
--- lib/xiao_ble_shell/xiao_ble_shell.c
+++ lib/xiao_ble_shell/xiao_ble_shell.c
@@ -1,71 +1,53 @@
 #include <zephyr/shell/shell.h>
+#include <app/lib/xiao_ble_shell.h>
 #include <date_time.h>
 #include <stdlib.h>
 
-static int cmd_demo_ping(const struct shell *sh, size_t argc, char **argv)
-{
-	ARG_UNUSED(argc);
-	ARG_UNUSED(argv);
+static char* get_text(int magic_value) {
+	switch (magic_value)
+	{
+	case ADAFRUIT_BOOTLOADER_UF2:
+		return "UF2";
+	case ADAFRUIT_BOOTLOADER_OTA:
+		return "OTA";
+	case ADAFRUIT_BOOTLOADER_SERIAL:
+		return "Serial";
 
-	shell_print(sh, "pong");
-
-	return 0;
+	default:
+		return NULL;
+	}
 }
 
-static int cmd_demo_board(const struct shell *sh, size_t argc, char **argv)
-{
-	ARG_UNUSED(argc);
-	ARG_UNUSED(argv);
+static int reset_into(const struct shell *sh, int magic_value) {
+	switch (magic_value)
+	{
+	case ADAFRUIT_BOOTLOADER_UF2:
+	case ADAFRUIT_BOOTLOADER_OTA:
+	case ADAFRUIT_BOOTLOADER_SERIAL:
+		break;
 
-	shell_print(sh, CONFIG_BOARD);
-
-	return 0;
-}
-
-static int cmd_demo_params(const struct shell *sh, size_t argc, char **argv)
-{
-	shell_print(sh, "argc = %zd", argc);
-	for (size_t cnt = 0; cnt < argc; cnt++) {
-		shell_print(sh, "  argv[%zd] = %s", cnt, argv[cnt]);
+	default:
+		shell_error(sh, "Wrong Magic Value used!");
+		return -EBADMSG;
 	}
 
+	// 0x57 UF2, 0xA8 OTA, 0x4e Serial
+	shell_print(sh, "Switching Xiao BLE to %s bootloader mode in 3", get_text(magic_value));
+	k_msleep(500);
+	shell_print(sh, "                                             2");
+	k_msleep(500);
+	shell_print(sh, "                                             1");
+	NRF_POWER->GPREGRET = magic_value;
+	NVIC_SystemReset();
 	return 0;
 }
-
-static int cmd_demo_hexdump(const struct shell *sh, size_t argc, char **argv)
-{
-	shell_print(sh, "argc = %zd", argc);
-	for (size_t cnt = 0; cnt < argc; cnt++) {
-		shell_print(sh, "argv[%zd]", cnt);
-		shell_hexdump(sh, argv[cnt], strlen(argv[cnt]));
-	}
-
-	return 0;
-}
-
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
-	SHELL_CMD(hexdump, NULL, "Hexdump params command.", cmd_demo_hexdump),
-	SHELL_CMD(params, NULL, "Print params command.", cmd_demo_params),
-	SHELL_CMD(ping, NULL, "Ping command.", cmd_demo_ping),
-	SHELL_CMD(board, NULL, "Show board name command.", cmd_demo_board),
-	SHELL_SUBCMD_SET_END /* Array terminated. */
-);
-SHELL_CMD_REGISTER(demo, &sub_demo, "Demo commands", NULL);
 
 static int cmd_uf2(const struct shell *sh, size_t argc, char **argv)
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	shell_print(sh, "Switching Xiao BLE to UF2 bootloader mode in 3");
-	k_msleep(500);
-	shell_print(sh, "                                             2");
-	k_msleep(500);
-	shell_print(sh, "                                             1");
-	NRF_POWER->GPREGRET = 0x57; // 0xA8 OTA, 0x4e Serial
-	NVIC_SystemReset();
-
-	return 0;
+	return reset_into(sh, ADAFRUIT_BOOTLOADER_UF2);
 }
 
 static int cmd_ota(const struct shell *sh, size_t argc, char **argv)
@@ -73,15 +55,7 @@ static int cmd_ota(const struct shell *sh, size_t argc, char **argv)
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	shell_print(sh, "Switching Xiao BLE to OTA bootloader mode in 3");
-	k_msleep(500);
-	shell_print(sh, "                                             2");
-	k_msleep(500);
-	shell_print(sh, "                                             1");
-	NRF_POWER->GPREGRET = 0xA8; // 0xA8 OTA, 0x4e Serial
-	NVIC_SystemReset();
-
-	return 0;
+	return reset_into(sh, ADAFRUIT_BOOTLOADER_OTA);
 }
 
 static int cmd_serial(const struct shell *sh, size_t argc, char **argv)
@@ -89,15 +63,7 @@ static int cmd_serial(const struct shell *sh, size_t argc, char **argv)
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	shell_print(sh, "Switching Xiao BLE to Serial bootloader mode in 3");
-	k_msleep(500);
-	shell_print(sh, "                                             2");
-	k_msleep(500);
-	shell_print(sh, "                                             1");
-	NRF_POWER->GPREGRET = 0x4E; // 0xA8 OTA, 0x4e Serial
-	NVIC_SystemReset();
-
-	return 0;
+	return reset_into(sh, ADAFRUIT_BOOTLOADER_SERIAL);
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_uf2,
