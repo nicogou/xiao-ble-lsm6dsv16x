50c5ec8a03a3ad84987c6b40966cc6812ecb0462
Nicolas Goualard <nicolas.goualard@sfr.fr>
2024-11-28 23:00:02 +0100

    Verbose mode to test CI

--- scripts/west_prepare_dfu.py
+++ scripts/west_prepare_dfu.py
@@ -56,8 +56,8 @@ over-the-air with the Bluefruit Connect app.''')
             os.system('rmdir /s /q ' + os.path.join(build_path, 'zephyr_ada_bl'))
             os.system('del ' + dest_path)
         elif platform.system() == 'linux':
-            os.system('cp ' + os.path.join(build_path, 'zephyr_ada_bl', 'zephyr.dat') + ' ' + os.path.join(build_path, 'zephyr.dat'))
-            os.system('rm -r ' + os.path.join(build_path, 'zephyr_ada_bl'))
-            os.system('rm ' + dest_path)
+            os.system('cp -v' + os.path.join(build_path, 'zephyr_ada_bl', 'zephyr.dat') + ' ' + os.path.join(build_path, 'zephyr.dat'))
+            os.system('rm -r -v' + os.path.join(build_path, 'zephyr_ada_bl'))
+            os.system('rm -v' + dest_path)
 
         return
